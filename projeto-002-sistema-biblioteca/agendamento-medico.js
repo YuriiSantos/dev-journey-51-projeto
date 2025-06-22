317bd1787dbccbb092b707e4b71ece38c3681cd6
Yuri Santos <yurii.rodrigues@outlook.com>
2025-06-21 23:52:25 -0300

    feat: Finalizando Sistema de agendamento clinico

--- projeto-002-sistema-biblioteca/agendamento-medico.js
+++ projeto-002-sistema-biblioteca/agendamento-medico.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -35,7 +36,7 @@ function listarMedicos(){
     } else {
         console.log("===== LISTA DE MÉDICOS =====");
         medicos.forEach((medico, index) => {
-            console.log(`${index + 1}`);
+            console.log(`${index + 1}. ${medico.nome}`);
             console.log(`Medico: ${medico.nome}`);
             console.log(`Especialidade: ${medico.especialidade}`);
             console.log(`Disponivel: ${medico.disponivel}`);
@@ -69,7 +70,7 @@ function listarPaciente(){
     } else {
         console.log("===== LISTA DE PACIENTES =====");
         pacientes.forEach((paciente, index) => {
-            console.log(`${index + 1}.`);
+            console.log(`${index + 1}.${paciente.nome}`);
             console.log(`Nome: ${paciente.nome}`);
             console.log(`Telefone: ${paciente.telefone}`);
             console.log(`Ativo: ${paciente.ativo}`);
@@ -79,6 +80,85 @@ function listarPaciente(){
     }
 }
 
+function agendarConsulta(){
+    rl.question("Qual id do paciente?", (id) =>{
+       const numeroId = parseInt(id);
+
+       const paciente = pacientes.find((c) => c.id === numeroId)
+
+       if (paciente){
+        console.log("Paciente cadastrado: ", paciente.nome);
+        rl.question("Qual o Id do Medico? ", (idMedico) => {
+            const numeroIdMedico = parseInt(idMedico);
+            const medico = medicos.find((c) => c.id === numeroIdMedico);
+
+            if (medico) {
+                if(medico.disponivel) {
+                    console.log("Medico Disponivel");
+                    
+                    const consulta = {
+                        id: consultas.length + 1,
+                        pacienteId: numeroId,
+                        medicoId: numeroIdMedico,
+                        dataConsulta: new Date(),
+                        status: "ativo"
+                    };
+
+                    consultas.push(consulta);
+
+                    medico.disponivel = false;
+
+                    console.log("Consulta Realizada!");
+                    iniciar();
+                } else { 
+                    console.log("Medico nao disponivel.") 
+                    iniciar();
+                }
+            } else {
+                console.log("Medico Nao encontrado")
+                iniciar();
+            }
+        })
+       } else {
+        console.log("Paciente nao cadastrad0!")
+        iniciar();
+       }
+    })
+}
+
+function listarConsultas(){
+    if (consultas.length === 0){
+        console.log("Nenhuma consulta encontrada!");
+        iniciar();
+        return;
+    } else {
+        console.log("===== LISTA DE CONSULTAS =====");
+        consultas.forEach((consulta, index) => {
+            const medico = medicos.find(u => u.id === consulta.medicoId);
+            const paciente = pacientes.find(l => l.id === consulta.pacienteId);
+
+            console.log(`${index + 1}. Consulta #${consulta.id}`);
+            console.log(` Paciente: ${paciente.nome}`);
+            console.log(` Medico: ${medico.nome}`);
+            console.log(` Status: ${consulta.status}`);
+            console.log("==============================");
+
+        });
+    }
+    iniciar()
+}
+
+
+function finalizarConsultas(){
+    const consultasAtivas = consultas.filter(e => e.status === "ativo");
+    
+    if (consultasAtivas.length === 0) {
+        console.log("Não há consultas ativas!");
+        iniciar();
+        return;
+    }
+    console.log("=========== CONSULTAS ATIVAS ===========")
+};
 
 function mostrarMenu(){
     console.log("\n ==== SISTEMA CLÍNICA MÉDICA ====");
@@ -86,6 +166,9 @@ function mostrarMenu(){
     console.log("2 - Cadastrar Paciente");
     console.log("3 - Listar Médicos");
     console.log("4 - Listar Pacientes");
+    console.log("5 - Agendar Consulta")
+    console.log("6 - Listar Consultas")
+    console.log("7 - Finalizar Consultas")
     console.log("0 - Sair")
 }
 
@@ -104,6 +187,15 @@ function iniciar(){
                 break;
             case "4" :
                 listarPaciente();
+                break;
+            case "5" :
+                agendarConsulta();
+                break;
+            case "6":
+                listarConsultas();
+                break;
+            case "7":
+                finalizarConsultas();
                 break;
             case "0" :
                 console.log("Saindo do sistema....");
--- projeto-002-sistema-biblioteca/biblioteca.js
+++ projeto-002-sistema-biblioteca/biblioteca.js
@@ -219,8 +219,6 @@ function devolverLivro(){
         iniciar();
         
     })
-
-
 }
 
 
